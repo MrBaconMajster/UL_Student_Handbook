c0971c0a3bd57cb447afae5e96c669ae58391b86
MACIEJ AUGUSTYNEK <18222757@studentmail.ul.ie>
2021-03-31 16:05:45 +0100

    Google maps updated

--- app/src/main/java/com/example/ulstudenthandbook/TimetableFrag.java
+++ app/src/main/java/com/example/ulstudenthandbook/TimetableFrag.java
@@ -38,7 +38,7 @@ public class TimetableFrag extends Fragment implements AdapterView.OnItemSelecte
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_PARAM2 = "param2-";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
