540a1cd7689a08ff376e524262b8da728a285bca
MACIEJ AUGUSTYNEK <18222757@studentmail.ul.ie>
2021-04-07 23:24:08 +0100

    Updated timetable display. -Allows more accurate timeslot representation -Reworked how its displayed

--- app/src/main/java/com/example/ulstudenthandbook/TimetableFrag.java
+++ app/src/main/java/com/example/ulstudenthandbook/TimetableFrag.java
@@ -31,6 +31,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.text.DateFormatSymbols;
 import java.time.DayOfWeek;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Locale;
 
@@ -51,6 +52,8 @@ public class TimetableFrag extends Fragment implements AdapterView.OnItemSelecte
     private String mParam2;
     private int dayDisplayed;
 
+    private TextView measureOfHeight;
+    private TextView measureOfHeight2;
     private TextView dayIndicator;
     private TextView entry9;
     private TextView entry10;
@@ -61,6 +64,19 @@ public class TimetableFrag extends Fragment implements AdapterView.OnItemSelecte
     private TextView entry15;
     private TextView entry16;
     private TextView entry17;
+    private TextView entry93;
+    private TextView entry103;
+    private TextView entry113;
+    private TextView entry123;
+    private TextView entry133;
+    private TextView entry143;
+    private TextView entry153;
+    private TextView entry163;
+    private TextView entry173;
+
+    private ArrayList<TextView> textViewArray = new ArrayList<>();
+
+    int entryDefaultHeight;
 
     private ImageView leftArrow;
     private ImageView rightArrow;
@@ -108,16 +124,48 @@ public class TimetableFrag extends Fragment implements AdapterView.OnItemSelecte
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_timetable, container, false);
 
+        measureOfHeight = view.findViewById(R.id.measureofheight);
+        measureOfHeight2 = view.findViewById(R.id.measureofheight2);
         dayIndicator = view.findViewById(R.id.dayIndicator);
-        entry9 = view.findViewById(R.id.Entry9);
-        entry10 = view.findViewById(R.id.Entry10);
-        entry11 = view.findViewById(R.id.Entry11);
-        entry12 = view.findViewById(R.id.Entry12);
-        entry13 = view.findViewById(R.id.Entry13);
-        entry14 = view.findViewById(R.id.Entry14);
-        entry15 = view.findViewById(R.id.Entry15);
-        entry16 = view.findViewById(R.id.Entry16);
-        entry17 = view.findViewById(R.id.Entry17);
+        entry9 = view.findViewById(R.id.entry9);
+        entry93 = view.findViewById(R.id.entry93);
+        entry10 = view.findViewById(R.id.entry10);
+        entry103 = view.findViewById(R.id.entry103);
+        entry11 = view.findViewById(R.id.entry11);
+        entry113 = view.findViewById(R.id.entry113);
+        entry12 = view.findViewById(R.id.entry12);
+        entry123 = view.findViewById(R.id.entry123);
+        entry13 = view.findViewById(R.id.entry13);
+        entry133 = view.findViewById(R.id.entry133);
+        entry14 = view.findViewById(R.id.entry14);
+        entry143 = view.findViewById(R.id.entry143);
+        entry15 = view.findViewById(R.id.entry15);
+        entry153 = view.findViewById(R.id.entry153);
+        entry16 = view.findViewById(R.id.entry16);
+        entry163 = view.findViewById(R.id.entry163);
+        entry17 = view.findViewById(R.id.entry17);
+        entry173 = view.findViewById(R.id.entry173);
+
+        textViewArray.add(entry9);
+        textViewArray.add(entry93);
+        textViewArray.add(entry10);
+        textViewArray.add(entry103);
+        textViewArray.add(entry11);
+        textViewArray.add(entry113);
+        textViewArray.add(entry12);
+        textViewArray.add(entry123);
+        textViewArray.add(entry13);
+        textViewArray.add(entry133);
+        textViewArray.add(entry14);
+        textViewArray.add(entry143);
+        textViewArray.add(entry15);
+        textViewArray.add(entry153);
+        textViewArray.add(entry16);
+        textViewArray.add(entry163);
+        textViewArray.add(entry17);
+        textViewArray.add(entry173);
+
+        entryDefaultHeight = entry9.getMeasuredHeight();
 
         leftArrow = view.findViewById(R.id.leftArrow);
         rightArrow = view.findViewById(R.id.rightArrow);
@@ -162,10 +210,11 @@ public class TimetableFrag extends Fragment implements AdapterView.OnItemSelecte
         timetableInfo = ReadTimetableInfoFromFile("Example4.txt");
         ScreenText = view.findViewById(R.id.textView3);
 
+
         //Sample entries !!!TO BE DELETED LATER!!!
         TimetableEntry entry = new TimetableEntry();
-        entry.timeStart = 9;
-        entry.timeEnd = 10;
+        entry.timeStart = 900;
+        entry.timeEnd = 1100;
         entry.building = "CS";
         entry.roomNo = "G001";
         entry.module = "CS4457";
@@ -174,18 +223,18 @@ public class TimetableFrag extends Fragment implements AdapterView.OnItemSelecte
         timetableInfo.SundayEntries.add(entry);
 
         TimetableEntry entry2 = new TimetableEntry();
-        entry2.timeStart = 15;
-        entry2.timeEnd = 10;
+        entry2.timeStart = 1500;
+        entry2.timeEnd = 1650;
         entry2.building = "CS";
         entry2.roomNo = "G001";
         entry2.module = "CS4457";
         entry2.colour = "Green";
 
-        timetableInfo.WednesdayEntries.add(entry2);
+        timetableInfo.SundayEntries.add(entry2);
 
         TimetableEntry entry3 = new TimetableEntry();
-        entry3.timeStart = 12;
-        entry3.timeEnd = 10;
+        entry3.timeStart = 1200;
+        entry3.timeEnd = 1300;
         entry3.building = "CS";
         entry3.roomNo = "G001";
         entry3.module = "CS4457";
@@ -194,8 +243,8 @@ public class TimetableFrag extends Fragment implements AdapterView.OnItemSelecte
         timetableInfo.SaturdayEntries.add(entry3);
 
         TimetableEntry entry4 = new TimetableEntry();
-        entry4.timeStart = 9;
-        entry4.timeEnd = 10;
+        entry4.timeStart = 900;
+        entry4.timeEnd = 1000;
         entry4.building = "CS";
         entry4.roomNo = "G001";
         entry4.module = "CS4457";
@@ -204,8 +253,8 @@ public class TimetableFrag extends Fragment implements AdapterView.OnItemSelecte
         timetableInfo.ThursdayEntries.add(entry4);
 
         TimetableEntry entry5 = new TimetableEntry();
-        entry5.timeStart = 16;
-        entry5.timeEnd = 10;
+        entry5.timeStart = 1600;
+        entry5.timeEnd = 1700;
         entry5.building = "CS";
         entry5.roomNo = "G001";
         entry5.module = "CS4457";
@@ -223,130 +272,119 @@ public class TimetableFrag extends Fragment implements AdapterView.OnItemSelecte
     }
 
     private void updateTimetable(int dayDisplayed) {
-        entry9.setText("");
-        entry10.setText("");
-        entry11.setText("");
-        entry12.setText("");
-        entry13.setText("");
-        entry14.setText("");
-        entry15.setText("");
-        entry16.setText("");
-        entry17.setText("");
 
-        entry9.setBackgroundResource(R.color.Grey);
-        entry10.setBackgroundResource(R.color.Grey);
-        entry11.setBackgroundResource(R.color.Grey);
-        entry12.setBackgroundResource(R.color.Grey);
-        entry13.setBackgroundResource(R.color.Grey);
-        entry14.setBackgroundResource(R.color.Grey);
-        entry15.setBackgroundResource(R.color.Grey);
-        entry16.setBackgroundResource(R.color.Grey);
-        entry17.setBackgroundResource(R.color.Grey);
+        entryDefaultHeight = measureOfHeight.getMeasuredHeight()+measureOfHeight2.getMeasuredHeight();
+
+        for ( TextView entry : textViewArray)
+        {
+            entry.setText("");
+            entry.setBackgroundResource(R.color.Grey);
+            entry.getLayoutParams().height = entryDefaultHeight;
+        }
 
         if (dayDisplayed == 1)
         {
             for ( TimetableEntry e : timetableInfo.SundayEntries)
             {
-                int startingTime = e.timeStart;
-                TextView entrySlot = findEntryWithTime(startingTime);
-                entrySlot.setText( e.module + "\n" + e.building + e.roomNo);
-                changeColour(entrySlot , e.colour);
-
+                updateEntry(e);
             }
         }
         else if (dayDisplayed == 2)
         {
             for ( TimetableEntry e : timetableInfo.MondayEntries)
             {
-                int startingTime = e.timeStart;
-                TextView entrySlot = findEntryWithTime(startingTime);
-                entrySlot.setText(e.module + "\n" + e.building + e.roomNo);
-                changeColour(entrySlot , e.colour);
-
+                updateEntry(e);
             }
         }
         else if (dayDisplayed == 3)
         {
             for ( TimetableEntry e : timetableInfo.TuesdayEntries)
             {
-                int startingTime = e.timeStart;
-                TextView entrySlot = findEntryWithTime(startingTime);
-                entrySlot.setText(e.module + "\n" + e.building + e.roomNo);
-                changeColour(entrySlot , e.colour);
-
+                updateEntry(e);
             }
         }
         else if (dayDisplayed == 4)
         {
             for ( TimetableEntry e : timetableInfo.WednesdayEntries)
             {
-                int startingTime = e.timeStart;
-                TextView entrySlot = findEntryWithTime(startingTime);
-                entrySlot.setText(e.module + "\n" + e.building + e.roomNo);
-                changeColour(entrySlot , e.colour);
+                updateEntry(e);
             }
         }
         else if (dayDisplayed == 5)
         {
             for ( TimetableEntry e : timetableInfo.ThursdayEntries)
             {
-                int startingTime = e.timeStart;
-                TextView entrySlot = findEntryWithTime(startingTime);
-                entrySlot.setText(e.module + "\n" + e.building + e.roomNo);
-                changeColour(entrySlot , e.colour);
+                updateEntry(e);
             }
         }
         else if (dayDisplayed == 6)
         {
             for ( TimetableEntry e : timetableInfo.FridayEntries)
             {
-                int startingTime = e.timeStart;
-                TextView entrySlot = findEntryWithTime(startingTime);
-                entrySlot.setText(e.module + "\n" + e.building + e.roomNo);
-                changeColour(entrySlot , e.colour);
+                updateEntry(e);
             }
         }
         else if (dayDisplayed == 7)
         {
             for ( TimetableEntry e : timetableInfo.SaturdayEntries)
             {
-                int startingTime = e.timeStart;
-                TextView entrySlot = findEntryWithTime(startingTime);
-                entrySlot.setText(e.module + "\n" + e.building + e.roomNo);
-                changeColour(entrySlot , e.colour);
+                updateEntry(e);
             }
         }
+    }
+
+    private void updateEntry(TimetableEntry e)
+    {
+        int startingTime = e.timeStart;
+        TextView entrySlot = findEntryWithTime(startingTime);
+        entrySlot.setText( e.module + "\n" + e.building + e.roomNo);
+        int slotsTaken = calculateSlotsTaken(e);
+        int indexOfE = textViewArray.indexOf(entrySlot);
+        entrySlot.getLayoutParams().height = entryDefaultHeight*slotsTaken;
+        for (int i=1; i<slotsTaken ; i++)
+        {
+            textViewArray.get(indexOfE+i).getLayoutParams().height= 0;
+        }
+        changeColour(entrySlot , e.colour);
+    }
+
+    private int calculateSlotsTaken(TimetableEntry e)
+    {
+        int start = e.timeStart;
+        int end = e.timeEnd;
+
+        return (end-start)/50;
     }
 
     private void changeColour(TextView entrySlot, String colour) {
 
         if(colour.equals("Red"))
         {
-            entrySlot.setBackgroundResource(R.color.Red);
+            entrySlot.setBackgroundResource(R.drawable.entry_drawable_red);
         }
         else if (colour.equals("Blue"))
         {
-            entrySlot.setBackgroundResource(R.color.Blue);
+            entrySlot.setBackgroundResource(R.drawable.entry_drawable_blue);
         }
         else if (colour.equals("Green"))
         {
-            entrySlot.setBackgroundResource(R.color.Green);
+            entrySlot.setBackgroundResource(R.drawable.entry_drawable_green);
         }
         else if (colour.equals("Orange"))
         {
-            entrySlot.setBackgroundResource(R.color.Orange);
+            entrySlot.setBackgroundResource(R.drawable.entry_drawable_orange);
         }
         else if (colour.equals("Pink"))
         {
-            entrySlot.setBackgroundResource(R.color.Pink);
+            entrySlot.setBackgroundResource(R.drawable.entry_drawable_pink);
         }
         else if (colour.equals("Black"))
         {
-            entrySlot.setBackgroundResource(R.color.Black);
+            entrySlot.setBackgroundResource(R.drawable.entry_drawable_black);
         }
         else if (colour.equals("Purple"))
         {
-            entrySlot.setBackgroundResource(R.color.Purple);
+            entrySlot.setBackgroundResource(R.drawable.entry_drawable_purple);
         }
         else
         {
@@ -357,41 +395,77 @@ public class TimetableFrag extends Fragment implements AdapterView.OnItemSelecte
 
     private TextView findEntryWithTime(int startingTime) {
         TextView entrySlot = entry9;
-        if(startingTime == 9)
+        if(startingTime == 900)
         {
             entrySlot = entry9;
         }
-        else if (startingTime == 10)
+        else if (startingTime == 1000)
         {
             entrySlot = entry10;
         }
-        else if (startingTime == 11)
+        else if (startingTime == 1100)
         {
             entrySlot = entry11;
         }
-        else if (startingTime == 12)
+        else if (startingTime == 1200)
         {
             entrySlot = entry12;
         }
-        else if (startingTime == 13)
+        else if (startingTime == 1300)
         {
             entrySlot = entry13;
         }
-        else if (startingTime == 14)
+        else if (startingTime == 1400)
         {
             entrySlot = entry14;
         }
-        else if (startingTime == 15)
+        else if (startingTime == 1500)
         {
             entrySlot = entry15;
         }
-        else if (startingTime == 16)
+        else if (startingTime == 1600)
         {
             entrySlot = entry16;
         }
-        else if (startingTime == 17)
+        else if (startingTime == 1700)
         {
             entrySlot = entry17;
+        }
+        else if (startingTime == 950)
+        {
+            entrySlot = entry93;
+        }
+        else if (startingTime == 1050)
+        {
+            entrySlot = entry103;
+        }
+        else if (startingTime == 1150)
+        {
+            entrySlot = entry113;
+        }
+        else if (startingTime == 1250)
+        {
+            entrySlot = entry123;
+        }
+        else if (startingTime == 1350)
+        {
+            entrySlot = entry133;
+        }
+        else if (startingTime == 1450)
+        {
+            entrySlot = entry143;
+        }
+        else if (startingTime == 1550)
+        {
+            entrySlot = entry153;
+        }
+        else if (startingTime == 1650)
+        {
+            entrySlot = entry163;
+        }
+        else if (startingTime == 1750)
+        {
+            entrySlot = entry173;
         }
 
         return entrySlot ;
